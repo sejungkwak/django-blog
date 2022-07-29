9afa890dde789ea15fcf73475ac8bbe3dc05f824
Sejung Kwak <kwak.sejung@gmail.com>
2022-07-29 22:53:11 +0000

    Add PostAdmin class

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {
+        'slug': ('title',)
+    }
+    list_filter = ('status', 'created_on')
+    summernote_field = ('content')
