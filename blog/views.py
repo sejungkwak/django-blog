9dd344a22586169976ef343932eccc1343be4ddc
Sejung Kwak <kwak.sejung@gmail.com>
2022-08-04 19:09:26 +0000

    fix: Correct typo

--- blog/views.py
+++ blog/views.py
@@ -17,7 +17,7 @@ class PostDetail(View):
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by('created_on')
         liked = False
-        if post.liked.filter(id=self.request.user.id).exists():
+        if post.likes.filter(id=self.request.user.id).exists():
             liked = True
         
         return render(
